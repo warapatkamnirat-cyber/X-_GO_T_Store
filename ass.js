86d98af8716b366358ced76efedfcfd7db707e03
warapatkamnirat-cyber <warapatkamnirat@gmail.com>
2026-08-05 23:59:42 +0700

    Update ass.js

--- ass.js
+++ ass.js
@@ -3,9 +3,9 @@ const ADMIN_STAFF_DATABASE = [
   {
     email: "PrimeStore@ass.co.th",
     pin: "260653",
-    redirect: "Boss.html",
+    redirect: "k9Xm2P7vL1qR8wN3zT5bY0uI4oE6aK9sD2fG8hJ1cV5nM7xW3Z.html",
     role: "IT"
   }
   // สามารถเพิ่มพนักงานหรือแอดมินคนอื่นได้ที่นี่ในอนาคต
-  // { email: "staff@ass.co.th", pin: "123456", redirect: "Boss.html", role: "Admin" }
+  // { email: "staff@ass.co.th", pin: "123456", redirect: "k9Xm2P7vL1qR8wN3zT5bY0uI4oE6aK9sD2fG8hJ1cV5nM7xW3Z.html", role: "Admin" }
 ];
